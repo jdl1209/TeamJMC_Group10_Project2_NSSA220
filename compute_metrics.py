df9eac21b459b4ead6571c1ec6f7015a18fec011
mwilliams70 <templehillride@gmail.com>
2022-11-17 17:03:57 -0500

    reading mistake

--- compute_metrics.py
+++ compute_metrics.py
@@ -16,8 +16,7 @@ def compute_metrics(node):
 
     request_bytes_sent=0
     request_bytes_received=0
-    reply_bytes_sent=0
-    reply_bytes_received=0
+
     """
     If Source is 192.168.100.1 and echo request == Request Sent, add that line to its own list
     If Destination is 192.168.100.1 and echo request == Request Recieved, add that line to its own list
@@ -62,12 +61,6 @@ def compute_metrics(node):
     for packet in req_received_list:
         request_bytes_received+=int(packet[5])
 
-    for packet in reply_sent_list:
-        reply_bytes_sent+=int(packet[5])
-        
-    for packet in reply_received_list:
-        reply_bytes_received+=int(packet[5])
-
     print("Requests Sent: " + str(request_sent))
     print("Requests Received: " + str(request_received))
     print("Replies Sent: " + str(reply_sent))
@@ -75,9 +68,7 @@ def compute_metrics(node):
 
     print("Echo Request Bytes Sent: " + str(request_bytes_sent))
     print("Echo Request Bytes Received: " + str(request_bytes_received))
-    print("Echo Reply Bytes Sent: " + str(reply_bytes_sent))
-    print("Echo Reply Bytes Received: " + str(reply_bytes_received))
-    
+
     
 
 compute_metrics("Node1_filtered.txt")
