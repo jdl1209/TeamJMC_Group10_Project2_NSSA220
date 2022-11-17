46ff37fe210fc9ab202cf4db66e7a5b217f0e1cd
mwilliams70 <templehillride@gmail.com>
2022-11-17 15:03:53 -0500

    Finds Request/Replies Sent & Received

--- compute_metrics.py
+++ compute_metrics.py
@@ -3,6 +3,9 @@ def compute_metrics(node):
     line = file.readline()
     i=0
     request_sent=0
+    request_received=0
+    reply_sent=0
+    reply_received=0
     request_data=0
     """
     If Source is 192.168.100.1 and echo request == Request Sent, add that line to its own list
@@ -19,14 +22,30 @@ def compute_metrics(node):
                 if item != "":
                     final.append(item)
             final[6:9] = [' '.join(final[6:9])]
-            request_data+=int(final[5])
-            request_sent+=1
+            if final[2]=="192.168.100.1" and final[6]=="Echo (ping) request":
+                request_sent+=1
+            if final[3]=="192.168.100.1" and final[6]=="Echo (ping) request":
+                request_received+=1
+            line=file.readline()
+        elif "Echo (ping) reply" in line:
+            line=line.strip().split(" ")
+            final=[]
+            for item in line:
+                if item != "":
+                    final.append(item)
+            final[6:9] = [' '.join(final[6:9])]
+            if final[2]=="192.168.100.1" and final[6]=="Echo (ping) reply":
+                reply_sent+=1
+            if final[3]=="192.168.100.1" and final[6]=="Echo (ping) reply":
+                reply_received+=1
             line=file.readline()
         else:
             line=file.readline()
 
     print("Requests Sent: " + str(request_sent))
-    print("Total Request Data: " + str(request_data))
+    print("Requests Received: " + str(request_received))
+    print("Replies Sent: " + str(reply_sent))
+    print("Replies Received: " + str(reply_received))
     
     
 
