bd3fb9adf846f89206d087fad8a51a06ab8a460d
mwilliams70 <templehillride@gmail.com>
2022-11-17 15:16:43 -0500

    Echo reply data sent/received has issues calculating

--- compute_metrics.py
+++ compute_metrics.py
@@ -1,10 +1,23 @@
 def compute_metrics(node):
     file = open(node)
     line = file.readline()
+
+    # Totals of Requests/Replies Sent & Recieved
     request_sent=0
     request_received=0
     reply_sent=0
     reply_received=0
+
+    # Lists for Requests/Replies Sent & Recieved
+    req_sent_list=[]
+    req_received_list=[]
+    reply_sent_list=[]
+    reply_received_list=[]
+
+    request_bytes_sent=0
+    request_bytes_received=0
+    reply_bytes_sent=0
+    reply_bytes_received=0
     """
     If Source is 192.168.100.1 and echo request == Request Sent, add that line to its own list
     If Destination is 192.168.100.1 and echo request == Request Recieved, add that line to its own list
@@ -21,8 +34,10 @@ def compute_metrics(node):
                     final.append(item)
             final[6:9] = [' '.join(final[6:9])]
             if final[2]=="192.168.100.1" and final[6]=="Echo (ping) request":
+                req_sent_list.append(final)
                 request_sent+=1
             if final[3]=="192.168.100.1" and final[6]=="Echo (ping) request":
+                req_received_list.append(final)
                 request_received+=1
             line=file.readline()
         elif "Echo (ping) reply" in line:
@@ -33,17 +48,35 @@ def compute_metrics(node):
                     final.append(item)
             final[6:9] = [' '.join(final[6:9])]
             if final[2]=="192.168.100.1" and final[6]=="Echo (ping) reply":
+                reply_sent_list.append(final)
                 reply_sent+=1
             if final[3]=="192.168.100.1" and final[6]=="Echo (ping) reply":
+                reply_received_list.append(final)
                 reply_received+=1
             line=file.readline()
         else:
             line=file.readline()
+    for packet in req_sent_list:
+        request_bytes_sent+=int(packet[5])
+
+    for packet in req_received_list:
+        request_bytes_received+=int(packet[5])
+
+    for packet in reply_sent_list:
+        reply_bytes_sent+=int(packet[5])
+        
+    for packet in reply_received_list:
+        reply_bytes_received+=int(packet[5])
 
     print("Requests Sent: " + str(request_sent))
     print("Requests Received: " + str(request_received))
     print("Replies Sent: " + str(reply_sent))
     print("Replies Received: " + str(reply_received))
+
+    print("Echo Request Bytes Sent: " + str(request_bytes_sent))
+    print("Echo Request Bytes Received: " + str(request_bytes_received))
+    print("Echo Reply Bytes Sent: " + str(reply_bytes_sent))
+    print("Echo Reply Bytes Received: " + str(reply_bytes_received))
     
     
 
