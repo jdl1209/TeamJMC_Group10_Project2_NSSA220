55e062973f25c00f2ef73d010ac0bccb7e7da1f4
jdl1209 <jdl1209@rit.edu>
2022-12-02 13:24:54 -0500

    should be final push

--- packet_analyzer.py
+++ packet_analyzer.py
@@ -2,6 +2,11 @@ import filter_packets
 import packet_parser
 import compute_metrics
 
+l1 = []
+l2 = []
+l3 = []
+l4 = []
+
 nodes = ["Node1.txt", "Node2.txt", "Node3.txt", "Node4.txt"]
 
 for node in nodes:
@@ -10,6 +15,9 @@ for node in nodes:
 
 nodes_filtered = ["Node1_filtered.txt", "Node2_filtered.txt", "Node3_filtered.txt", "Node4_filtered.txt"]
 addresses = ["192.168.100.1", "192.168.100.2", "192.168.200.1", "192.168.200.2"]
+
+f = open("Project2_Output_TeamJMC.csv", "w")
+f.close()
 
 i = 0
 for filtered in nodes_filtered:
@@ -24,7 +32,12 @@ for filtered in nodes_filtered:
 
     packet_parser.packet_parser(filtered, addresses[i], l1, l2, l3, l4)
 
-    print(str(node_name))
+    print(str(node_name) + "\n")
+
+    f = open("Project2_Output_TeamJMC.csv", "a")
+    f.write(str(node_name))
+    f.close()
+    
     compute_metrics.compute_metrics(l1, l2, l3, l4)
 
     i += 1
